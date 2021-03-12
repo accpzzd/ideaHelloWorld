71cee78174aaa299b927f045661761a42d5859c7
java1234 <java1234@qq.com>
2021-03-12 12:13:16 +0800

    第一次代码提交

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @author java1234_小锋
+ * @site www.java1234.com
+ * @company 南通小锋网络科技有限公司
+ * @create 2021-03-12 12:10
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
